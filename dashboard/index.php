c24cffcb288f9b2c45c0822686191903416c6804
Jai Shankar <Shankarjai.g@gmail.com>
2024-12-21 11:26:59 +0530

    Basic Sidebar Done - rewritten code in a modular way.

---
+++ dashboard/index.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>Dashboard</title>
+        <link rel="stylesheet" href="css/style.css"></link>
+        <link rel="stylesheet" href="../utility/lib/reset.css"></link>
+    </head>
+
+
+    <body>
+
+        <div class="">
+
+        </div>
+
+        <?php
+            include 'template/sidebar.php'
+        ?>
+        
+        <?php
+            include 'template/mainbar.php'
+        ?>
+
+        
+        
+    </body>
+
+</html>
---
+++ dashboard/template/sidebar.php
@@ -0,0 +1,19 @@
+<div class="side-bar-container">
+
+    <div class="side-bar-menus">
+
+        <div class="side-bar-menu">
+            <p>Vitamins</p>
+        </div>
+
+        <div class="side-bar-menu">
+            <p>Food</p>
+        </div>
+
+        <div class="side-bar-menu">
+            <p>Demo</p>
+        </div>
+
+    </div>
+
+</div>
