28476c3d2fd6d8ffc0a9363e8cd8037b41efc132
Jai Shankar <Shankarjai.g@gmail.com>
2025-02-22 19:37:53 +0530

    Done listing out the vitamin list from DB

--- dashboard/nutrients.php
+++ dashboard/nutrients.php
@@ -20,62 +20,89 @@
             $pdo = new PDO("mysql:host=$host;dbname=$dbname", "$user", "$password");
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+            $sql = "SELECT nutrients.id AS nutrient_id, nutrients.name AS nutrient_name, nutrient_pros.pro, nutrient_pros.pro_id
+                    FROM nutrients
+                    LEFT JOIN nutrient_pros ON nutrients.id = nutrient_pros.nutrient_id";
 
-            $sql = "SELECT * FROM nutrients";
 
-             // Prepare and execute the query
             $stmt = $pdo->prepare($sql);
             $stmt->execute();
 
-            // Fetch all results as an associative array
-            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            
-            foreach($results as $row){
-                echo('<p>'.$row['id'].$row['name'].$row['desc'].'</p>');
+            $nutrients = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            $results = [];
+
+            print_r($nutrients) ;
+
+
+            foreach($nutrients as $row){
+
+                $nutrient_id = $row['nutrient_id'];
+                $pro_id = $row['pro_id'];
+
+                 // Second query: Get variations for this pro_id
+                $variationStmt = $pdo->prepare("SELECT variation_text FROM pro_variation WHERE pro_id = ?");
+                $variationStmt->execute([$pro_id]);
+                $variations = $variationStmt->fetchAll(PDO::FETCH_COLUMN);
+
+                // Store data in an array
+                $results[$nutrient_id]['name'] = $row['nutrient_name'];
+                $results[$nutrient_id]['pros'][] = [
+                    'pro' => $row['pro'],
+                    'variations' => $variations
+                ];
+
             }
 
-            $sql = "SELECT 
-                nutrients.name as nutrient_name, 
-                nutrient_pros.pro,
-                pro_variation.variation_text
+            foreach($results as $_row){
+
+
+                echo('
                 
-            FROM nutrients
-            LEFT JOIN nutrient_pros on nutrients.id = nutrient_pros.nutrient_id
-            LEFT JOIN pro_variation on nutrient_pros.pro_id = pro_variation.pro_id";
+                    <div class="nutrient-card">
+                        <div class="nutrient-card-header">
+                            <button class="button nutrition-toggle">X</button>  
+                                <span class="nutrient-card-title">'.$_row['name'].'</span>
+                            <button class="button modify">modify</button>
+                            <button class="button delete">delete</button>
+                        </div>
+
+                        <div class="nutrient-card-body">
+
+                        
+                            
+                ');
 
 
-            $stmt = $pdo->prepare($sql);
-            $stmt->execute();
+                foreach($_row['pros'] as $_pros){
 
-            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                    echo('
+                            
+                            <div class="pros-box desc-card">
+                            <p>'.$_pros['pro'].'</p>
+                            <div class="pros-variation-wrapper">
+                                
+                    ');
 
-            print_r($results) ;
+                    foreach($_pros['variations'] as $_variation ){
 
-            foreach($results as $row){
+                        echo('
 
-                echo ('  <div class="nutrient-card">
-            
-                            <div class="nutrient-card-header">
-                                <button class="button nutrition-toggle">X</button>  
-                                <span class="nutrient-card-title">'.$row['nutrient_name'].'</span>
-                                <button class="button modify">modify</button>
-                                <button class="button delete">delete</button>
+                            <div class="pro-variation desc-card">
+                                <p>'.$_variation.'</p>
                             </div>
                         
-                            <div class="nutrient-card-body">
-                                <div class="pros-box desc-card">
-                                    <p>'.$row['pro'].'</p>
+                        ');
+                    }
 
-                                </div>
-                                <div class="cons-box desc-card">
-                                    <p>the nutrient cons will be gone here </p>
+                    echo('</div>'); // closed pro-variation-wrapper
+                    echo('</div>'); // closed pros-box desc-card
 
-                                </div>
-                            </div>
-                        
+                }
 
-                        </div>');
                 
+                echo('</div>'); // closed nutrient-card-body
+                echo('</div>'); // closed nutrient-card
 
             }
 
